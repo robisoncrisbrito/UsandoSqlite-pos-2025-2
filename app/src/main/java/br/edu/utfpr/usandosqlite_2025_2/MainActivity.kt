df9697107f5a5f2135bc1a5e58b7b2e61ef3f7f7
Robison Cris Brito <44240574+robisoncrisbrito@users.noreply.github.com>
2025-11-29 10:08:10 -0300

    Projeto com o CRUD completo

--- app/src/main/java/br/edu/utfpr/usandosqlite_2025_2/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/usandosqlite_2025_2/MainActivity.kt
@@ -1,8 +1,11 @@
 package br.edu.utfpr.usandosqlite_2025_2
 
+import android.content.ContentValues
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,7 +15,6 @@ import br.edu.utfpr.usandosqlite_2025_2.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     private lateinit var banco: SQLiteDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,10 +24,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        banco = SQLiteDatabase.openOrCreateDatabase(
-            "bdfile.sqlite",
+        banco = openOrCreateDatabase(
+            "bdfile.sqlite", MODE_PRIVATE,
             null
         )
+
+        banco.execSQL( "CREATE TABLE IF NOT EXISTS cadastro (_id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, telefone TEXT )" )
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -35,9 +39,105 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun btIncluirOnClick(view: View) {}
-    fun btAlterarOnClick(view: View) {}
-    fun btExcluirOnClick(view: View) {}
-    fun btPesquisarOnClick(view: View) {}
-    fun btListarOnClick(view: View) {}
+    fun btIncluirOnClick(view: View) {
+
+        val registro = ContentValues()
+        registro.put("nome", binding.etNome.text.toString())
+        registro.put("telefone", binding.etTelefone.text.toString())
+
+        banco.insert( "cadastro", null, registro)
+
+        Toast.makeText(
+            this,
+            "Inclusão efetuada com Sucesso.",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    fun btAlterarOnClick(view: View) {
+        val registro = ContentValues()
+        registro.put("nome", binding.etNome.text.toString())
+        registro.put("telefone", binding.etTelefone.text.toString())
+
+        banco.update( "cadastro", registro, "_id = ${binding.etCod.text.toString()}", null )
+
+        Toast.makeText(
+            this,
+            "Alteração efetuada com Sucesso.",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+    fun btExcluirOnClick(view: View) {
+        banco.delete( "cadastro", "_id = ${binding.etCod.text.toString()}", null )
+
+        Toast.makeText(
+            this,
+            "Exclusão efetuada com Sucesso.",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+    fun btPesquisarOnClick(view: View) {
+
+        val registro = banco.query(
+            "cadastro",
+            null,
+            "_id = ${binding.etCod.text.toString()}",
+            null,
+            null,
+            null,
+            null
+        )
+
+        if ( registro.moveToNext() ) {
+
+            val nome = registro.getString(1 )
+            val telefone = registro.getString( 2 )
+
+            binding.etNome.setText( nome )
+            binding.etTelefone.setText( telefone )
+
+        } else {
+
+            binding.etNome.setText( "" )
+            binding.etTelefone.setText( "" )
+
+            Toast.makeText(
+                this,
+                "Registro não encontro.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+
+    }
+    fun btListarOnClick(view: View) {
+
+        val registros = banco.query(
+            "cadastro",
+            null,
+            null,
+            null,
+            null,
+            null,
+            null
+        )
+
+        val saida = StringBuilder()
+
+        while ( registros.moveToNext() ) {
+            val nome = registros.getString(1 )
+            val telefone = registros.getString( 2 )
+
+            saida.append( " ${nome} - ${telefone} \n " )
+
+        }
+
+        Toast.makeText(
+            this,
+            saida.toString(),
+            Toast.LENGTH_SHORT
+        ).show()
+
+
+    }
 }
