ac8b500df76ddbd48677bf363ee009d9565d3604
Robison Cris Brito <44240574+robisoncrisbrito@users.noreply.github.com>
2025-11-29 11:32:03 -0300

    Projeto com a classe de entidade (Cadastro.kt) e a classe de Model (DatabaseHandler)

---
+++ app/src/main/java/br/edu/utfpr/usandosqlite_2025_2/database/DatabaseHandler.kt
@@ -0,0 +1,96 @@
+package br.edu.utfpr.usandosqlite_2025_2.database
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.widget.Toast
+import br.edu.utfpr.usandosqlite_2025_2.entity.Cadastro
+
+class DatabaseHandler(context: Context) : SQLiteOpenHelper( context, DATABASE_NAME, null, DATABASE_VERSION ) {
+
+    companion object {
+        const val DATABASE_VERSION = 1
+        const val DATABASE_NAME = "bdfile.sqlite"
+        const val TABLE_NAME = "cadastro"
+        const val COLUMN_ID = "0"
+        const val COLUMN_NOME = "0"
+        const val COLUMN_TELEFONE = "0"
+    }
+
+    override fun onCreate(banco: SQLiteDatabase?) {
+        banco?.execSQL( "CREATE TABLE IF NOT EXISTS $TABLE_NAME (_id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, telefone TEXT, email TEXT )" )
+    }
+
+    override fun onUpgrade(
+        banco: SQLiteDatabase?,
+        oldVerstion: Int,
+        newVersion: Int
+    ) {
+        banco?.execSQL( "DROP TABLE IF EXISTS $TABLE_NAME")
+        onCreate(banco)
+    }
+
+
+    fun inserir(cadastro: Cadastro) {
+        val registro = ContentValues()
+        registro.put("nome", cadastro.nome)
+        registro.put("telefone", cadastro.telefone)
+
+        writableDatabase.insert( TABLE_NAME, null, registro)
+    }
+
+    fun alterar(cadastro: Cadastro) {
+        val registro = ContentValues()
+        registro.put("nome", cadastro.nome)
+        registro.put("telefone", cadastro.telefone)
+
+        writableDatabase.update( TABLE_NAME, registro, "_id = ${cadastro._id}", null )
+    }
+
+    fun excluir(id:Int) {
+        writableDatabase.delete( TABLE_NAME, "_id = $id", null )
+    }
+
+    fun pesquisar(id:Int): Cadastro? {
+
+        val registro = writableDatabase.query(
+            TABLE_NAME,
+            null,
+            "_id = $id",
+            null,
+            null,
+            null,
+            null
+        )
+
+        var retorno: Cadastro? = null
+
+        if (registro.moveToNext() ) {
+
+            val nome = registro.getString(1)
+            val telefone = registro.getString(2)
+
+            retorno = Cadastro(id, nome, telefone)
+
+        }
+
+        return retorno
+    }
+
+    fun listar(): Cursor {
+        val registros = writableDatabase.query(
+            TABLE_NAME,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null
+        )
+
+        return registros
+    }
+
+}
---
+++ app/src/main/java/br/edu/utfpr/usandosqlite_2025_2/entity/Cadastro.kt
@@ -0,0 +1,7 @@
+package br.edu.utfpr.usandosqlite_2025_2.entity
+
+data class Cadastro(
+    val _id: Int,
+    val nome: String,
+    val telefone: String
+)
