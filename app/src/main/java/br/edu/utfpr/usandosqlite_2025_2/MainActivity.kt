799ac2d523d50d08a5dcadbce09a6e42ca193230
Robison Cris Brito <44240574+robisoncrisbrito@users.noreply.github.com>
2025-11-29 08:52:05 -0300

    Initial commit com a Interface Gráfica desenvolvida

---
+++ app/src/main/java/br/edu/utfpr/usandosqlite_2025_2/MainActivity.kt
@@ -0,0 +1,27 @@
+package br.edu.utfpr.usandosqlite_2025_2
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun btIncluirOnClick(view: View) {}
+    fun btAlterarOnClick(view: View) {}
+    fun btExcluirOnClick(view: View) {}
+    fun btPesquisarOnClick(view: View) {}
+    fun btListarOnClick(view: View) {}
+}
